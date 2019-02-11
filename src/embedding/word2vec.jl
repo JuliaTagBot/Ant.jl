091f44d25275178fe3eef9aaeae56b60e8fa2d97
del2z <delta.z@aliyun.com>
2019-02-11 13:48:46 +0800

    wip: test flux

--- src/embedding/word2vec.jl
+++ src/embedding/word2vec.jl
@@ -3,3 +3,15 @@
 
 using Flux
 
+W = rand(2, 5)
+b = rand(2)
+
+predict(x) = W*x .+ b
+
+function loss(x, y)
+  ŷ = predict(x)
+  sum((y .- ŷ).^2)
+end
+
+x, y = rand(5), rand(2) # Dummy data
+loss(x, y) # ~ 3
