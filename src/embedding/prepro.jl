0dfb8ef56709f06df3cb2175e7de87a11243024f
del2z <delta.z@aliyun.com>
2019-02-19 20:15:05 +0800

    wip: word2vec model

--- src/embedding/prepro.jl
+++ src/embedding/prepro.jl
@@ -1,13 +1,34 @@
 #!/usr/bin/env julia
 # del2z <delta.z@aliyun.com>
 
-module Word2Vec
+using StatsBase
 
-using Flux
-using JSON
+" Load raw corpus in `Vector{String}` "
+function loaddata(fname::AbstractString)
+    corpus = Vector{String}()
+    open(fname, "r") do fin
+        for line in eachline(fin)
+            length(line) > 10 && push!(corpus, strip(line))
+        end
+    end
+    corpus
+end
 
-const config = JSON.parsefile(joinpath(@__DIR__, "word2vec.json"))
+" Segment words in corpus "
+function segment!(corpus::Vector{<:AbstractString})
+    newcorp = Vector{Vector{String}}()
+    for k in 1:length(corpus)
+        push!(newcorp, string.(collect(corpus[k])))
+    end
+    newcorp
+end
 
-W = param(rand())
-
+" Generate vocabulary from corpus "
+function genvocab(corpus::Vector{Vector{String}}, mincount::Integer = 0)
+    wordcount = Dict{String,Int64}()
+    for k in 1:length(corpus)
+        addcounts!(wordcount, corpus[k])
+    end
+    (mincount <= 0) ? collect(keys(wordcount)) :
+    filter(w -> wordcount[w] >= mincount, collect(keys(wordcount)))
 end
--- src/embedding/word2vec.jl
+++ src/embedding/word2vec.jl
@@ -5,9 +5,34 @@ module Word2Vec
 
 using Flux
 using JSON
+using DataStructures
 
 const config = JSON.parsefile(joinpath(@__DIR__, "word2vec.json"))
 
-W = param(rand())
+include("prepro.jl")
+
+corpus = segment!(loaddata("corpus.txt"))
+idx2word = genvocab(corpus, 1)
+pushfirst!(idx2word, "<NIL>", "<UNK>")
+word2idx = OrderedDict(zip(idx2word, 1:length(idx2word)))
+@info length(corpus)
+@info length(idx2word), typeof(idx2word)
+@info length(word2idx), word2idx["<NIL>"], word2idx["<UNK>"]
+
+# negative sampling
+W_emb = param(rand(length(idx2word), config["model"]["embed_size"]))
+W_out = param(rand())
+b_out = param(rand())
+
+model = nothing
+
+function loss()
+end
+
+function train!()
+end
+
+function test()
+end
 
 end
